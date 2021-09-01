423a4413749b8aed5a1d4edec1f2237b994ec9b5
Camilo Pérez <camilo.perez@unholster.com>
2021-09-01 15:28:17 -0400

    Add verifier method to Android interface

--- interfaces/android/src/lib.rs
+++ interfaces/android/src/lib.rs
@@ -5,10 +5,14 @@ pub mod bulletproofs_android {
     extern crate jni;
     extern crate bulletproofs_gadgets;
 
+    use std::convert::TryFrom;
+
     use jni::JNIEnv;
     use jni::objects::{JClass, JObject, JString, JValue};
+    use jni::sys::{jbyteArray, jboolean};
 
     use bulletproofs_gadgets::prove::prove;
+    use bulletproofs_gadgets::verify::verify;
 
     //
     // Signature for the BulletproofWrapper used in the Android side (output of javap -s)
@@ -33,6 +37,8 @@ pub mod bulletproofs_android {
 
         public com.unholster.examplebulletproofs.BulletproofWrapper(java.lang.String, java.lang.String, java.lang.String, java.lang.String);
             descriptor: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V
+        public com.unholster.examplebulletproofs.BulletproofWrapper(java.lang.String, java.lang.String, java.lang.String, java.lang.String, byte[]);
+            descriptor: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;[B)V
 
         public java.lang.String getName();
             descriptor: ()Ljava/lang/String;
@@ -61,18 +67,24 @@ pub mod bulletproofs_android {
     }
     */
 
-    fn get_java_object_member(env: &JNIEnv, object: JObject, getter: &str, signature: &str) -> Result<String, Box<dyn std::error::Error>> {
+    fn get_jobject_member_string(env: &JNIEnv, object: JObject, getter: &str, signature: &str) -> Result<String, Box<dyn std::error::Error>> {
         let jvalue: JValue = env.call_method(object, getter, signature, &[])?;
         let jstring: JString = jvalue.l()?.into();
         Ok(env.get_string(jstring)?.into())
     }
 
+    fn get_jobject_member_bytes(env: &JNIEnv, object: JObject, getter: &str, signature: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let jvalue: JValue = env.call_method(object, getter, signature, &[])?;
+        let jarray: jbyteArray = jvalue.l()?.into_inner();  // jbyteArray = jarray = jobject
+        Ok(env.convert_byte_array(jarray)?)
+    }
+
     #[no_mangle]
     pub unsafe extern fn Java_com_unholster_examplebulletproofs_RustBulletproofs_extProve(env: JNIEnv, _: JClass, data: JObject) {
-        let name: String = get_java_object_member(&env, data, "getName", "()Ljava/lang/String;").unwrap();
-        let instance: String = get_java_object_member(&env, data, "getInstance", "()Ljava/lang/String;").unwrap();
-        let witness: String = get_java_object_member(&env, data, "getWitness", "()Ljava/lang/String;").unwrap();
-        let gadgets: String = get_java_object_member(&env, data, "getGadgets", "()Ljava/lang/String;").unwrap();
+        let name: String = get_jobject_member_string(&env, data, "getName", "()Ljava/lang/String;").unwrap();
+        let instance: String = get_jobject_member_string(&env, data, "getInstance", "()Ljava/lang/String;").unwrap();
+        let witness: String = get_jobject_member_string(&env, data, "getWitness", "()Ljava/lang/String;").unwrap();
+        let gadgets: String = get_jobject_member_string(&env, data, "getGadgets", "()Ljava/lang/String;").unwrap();
         let mut commitments = String::new();
         let proof = prove(Box::leak(name.into_boxed_str()), instance, witness, gadgets, &mut commitments).expect("unable to generate proof from provided Android data");
 
@@ -80,5 +92,17 @@ pub mod bulletproofs_android {
         env.call_method(data, "setCommitments", "(Ljava/lang/String;)V", &[JValue::from(java_commitments)]).unwrap();
         let java_proof: JObject = JObject::from(env.byte_array_from_slice(&proof[..]).unwrap());
         env.call_method(data, "setProof", "([B)V", &[JValue::from(java_proof)]).unwrap();
+    }
+
+    #[no_mangle]
+    pub unsafe extern fn Java_com_unholster_examplebulletproofs_RustBulletproofs_extVerify(env: JNIEnv, _: JClass, data: JObject) -> jboolean {
+        let name: String = get_jobject_member_string(&env, data, "getName", "()Ljava/lang/String;").unwrap();
+        let instance: String = get_jobject_member_string(&env, data, "getInstance", "()Ljava/lang/String;").unwrap();
+        let commitments: String = get_jobject_member_string(&env, data, "getCommitments", "()Ljava/lang/String;").unwrap();
+        let gadgets: String = get_jobject_member_string(&env, data, "getGadgets", "()Ljava/lang/String;").unwrap();
+        let proof: Vec<u8> = get_jobject_member_bytes(&env, data, "getProof", "()[B").unwrap();
+
+        let verified = verify(Box::leak(name.into_boxed_str()), instance, proof, commitments, gadgets).expect("unable to verify proof from provided Android data");
+        jboolean::try_from(JValue::from(verified)).unwrap()
     }
 }
